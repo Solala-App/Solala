1eed8ac985503107834049ad55cc6d7e6f76c294
leeja <leejanzen91@gmail.com>
2022-07-18 12:27:38 -0700

    updated styles

--- App.tsx
+++ App.tsx
@@ -1,7 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import {Text, View } from 'react-native';
-import styles from './Style'
 import {Test} from "./solala-app/screens"
 
 
--- solala-app/components/card.js
+++ solala-app/components/card.js
@@ -19,6 +19,64 @@ const styles = StyleSheet.create({
     display: "flex",
     ...shadowProp,
   },
+
+  cardHeader: {
+    display: "flex",
+    justifyContent: "center",
+    flexDirection: "row",
+    alignItems: "center",
+    fontSize: 30,
+    fontFamily: "Montserrat Alternates",
+    fontWeight: "bold",
+    /*font-family: 'Courgette', cursive,*/
+    color: "white",
+    position: "absolute",
+    borderRadius: 30,
+    backgroundColor: "#126760",
+    opacity: 70,
+  },
+
+  cardItem: {
+    backgroundColor: "#E3FFEB",
+    width: 400,
+    borderRadius: 30,
+    height: 20,
+    padding: 20,
+    fontSize: 24,
+    color: "#126760",
+    fontFamily: "Montserrat Alternates",
+    fontWeight: "bold",
+  },
+
+  button: {
+    display: "flex",
+    justifyContent: "space-around",
+    flexDirection: "column",
+    height: 220,
+    width: 500,
+    alignItems: "center",
+    paddingTop: 70,
+  },
+
+  cardObjectLeft: {
+    /*checkbox bubble, time, reset, ect */
+    paddingRight: 50,
+  },
+
+  scrollDown: {
+    height: 30,
+    width: 50,
+    alignContent: "center",
+    paddingTop: 10,
+    paddingRight: 15,
+  },
+
+  addButton: {
+    height: 50,
+    width: 50,
+    position: "absolute",
+    left: 430,
+  },
 });
 
 export default function homepage() {

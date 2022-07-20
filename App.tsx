7d83c4693fea6ebcffdb3d5cd5c4ef59c36a66d8
EdisonLeung <edisonleung2003@outlook.com>
2022-07-20 00:01:27 -0700

    implemented footer component

--- App.tsx
+++ App.tsx
@@ -1,16 +1,12 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { Text, View } from "react-native";
-import Test from "./solala-app/screens/Test";
-import Homepage from "./solala-app/screens/Homepage";
+import Homepage from "./solala-app/screens/homepage.js";
 import { Card } from "./solala-app/components";
 
 export default function App() {
   //add a splashscreen
-  return (
-    <View>
-      {/* <Test /> */}
-      <Homepage />
-    </View>
+    return (
+        <Homepage/>
   );
 }
--- solala-app/screens/homepage.js
+++ solala-app/screens/homepage.js
@@ -1,29 +1,34 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
+import Footer from "../components/Footer.js"
 
 export default function Homepage() {
-  return (
-    <View>
-      <View style={styles.header}></View>
-      <View style={styles.mainView}>
-        <View style={styles.eventView}>
-          <View style={styles.card}></View>
-          <View style={styles.card}></View>
-        </View>
+    return (
+        <View style={styles.container}>
+          <View style={styles.header}></View>
+          <View style={styles.mainView}>
+              <View style={styles.eventView}>
+                  <View style={styles.card}></View>
+                  <View style={styles.card}></View>
+              </View>
 
-        <View style={styles.solalaView}></View>
+              <View style={styles.solalaView}></View>
 
-        <View style={styles.healthView}>
-          <View style={styles.card}></View>
-          <View style={styles.card}></View>
-        </View>
+              <View style={styles.healthView}>
+                  <View style={styles.card}></View>
+                  <View style={styles.card}></View>
+              </View>
+          </View>
+          <View style={styles.footer}><Footer/></View>
       </View>
-      <View style={styles.footer}></View>
-    </View>
   );
 }
 
 const styles = StyleSheet.create({
+    container: {
+      flex:1,
+      height:400  
+    },
   header: {
     display: "flex",
     flexDirection: "row",
@@ -40,13 +45,15 @@ const styles = StyleSheet.create({
     alignItems: "center",
     width: "100%",
   },
-  footer: {
-    display: "flex",
-    flexDirection: "row",
-    JustifyContent: "space-between",
-    backgroundColor: "#CB7236",
-
-    width: "100%",
+    footer: {
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        width: '100%',
+        height: 100,
+        bottom: 0,
+        position: 'absolute',
+        backgroundColor: "#CB7236"
   },
   eventView: {
     display: "flex",

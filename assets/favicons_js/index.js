0e533aefc8e2e8610ebaf535d050fcb822555898
Lee Janzen-Morel <leejanzen91@gmail.com>
2022-07-20 16:33:26 -0700

    added js svg files to asests

---
+++ assets/favicons_js/index.js
@@ -0,0 +1,14 @@
+export { default as Calendar } from "./Calendar";
+export { default as Cancel } from "./Cancel";
+export { default as Check } from "./Check";
+export { default as CheckLeaf } from "./CheckLeaf";
+export { default as Greenhouse } from "./Greenhouse";
+export { default as Minus } from "./Minus";
+export { default as Plus } from "./Plus";
+export { default as ScrollDown } from "./ScrollDown";
+export { default as ScrollLeft } from "./ScrollLeft";
+export { default as ScrollRight } from "./ScrollRight";
+export { default as ScrollUp } from "./ScrollUp";
+export { default as Swipe } from "./Swipe";
+export { default as ToDoList } from "./ToDoList";
+export { default as Undo } from "./Undo";

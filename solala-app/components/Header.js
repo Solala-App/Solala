facea401c224db330c13eae8b2f9ece68f5e8906
Christopher <cr0y@uw.edu>
2022-07-20 19:59:13 -0700

    Alright, so I fixed the header for the most part. It shows up when called as a component and I have placeholders for the images until we get those set up. Tomorrow, I'll make it so the header stays in palce at the top of the screen when someone is scrolling always and will also reimplement the props since I took those out currently.

--- solala-app/components/Header.js
+++ solala-app/components/Header.js
@@ -1,107 +1,93 @@
 import React from "react";
-import {
-  StyleSheet,
-  Text,
-  View,
-  Image,
-  TouchableOpacity,
-  Platform,
-} from "react-native";
+import { Text, View, StyleSheet, TouchableOpacity, Platform } from "react-native";
+
+import GreenHouse from "../../assets/favicons_light/greenhouse.svg";
+import Calendar from "../../assets/favicons_light/calendar.svg";
+import ToDoList from "../../assets/favicons_light/to-do-list.svg";
+import { theme } from "../constants";
 import { RFValue } from "react-native-responsive-fontsize";
 import * as Favicon from "../../assets/favicons_js";
 
-export default function Header(props) {
-  let staticImage = require("./Rain.png");
+const { colorPalette } = theme;
 
-  if (Platform.OS === "web") {
+const Header = () => {
+
+
     return (
-      <View style={styles.header}>
-        <TouchableOpacity>
-          <Favicon.Calendar />
-        </TouchableOpacity>
+        <View style={styles.header}>
 
-        <View style={styles.headerSun}>
-          <Text style={styles.textBubble}>{props.time}</Text>
+            <TouchableOpacity>
+                <View style={styles.placeholder}></View>
+            </TouchableOpacity>
 
-          <Image
-            style={{
-              top: RFValue(75),
-              left: RFValue(25),
-              width: RFValue(50),
-              height: RFValue(50),
-            }}
-            source={require("./Rain.png")}
-          />
+            <View style={styles.headerSun}>
+                <Text style={styles.textBubble}>"Good Morning, Bring Your Umbrella Today!</Text>
+                <View style={styles.placeholderWeather}></View>
+                <Text style={styles.announcement}> "It's Neha's Birthday Today!"</Text>
+            </View>
 
-          <Text style={styles.announcement}>{props.announcement}</Text>
+            <TouchableOpacity>
+                <View style={styles.placeholder}></View>
+            </TouchableOpacity>
+
         </View>
-
-        <TouchableOpacity>
-          <Image
-            style={{
-              width: RFValue(25),
-              height: RFValue(25),
-            }}
-            source={require("./TODO.png")}
-          />
-        </TouchableOpacity>
-      </View>
     );
-  } else if (Platform.OS === "android") {
-    <View style={styles.headerSun}>
-      <Text style={styles.textBubble}>{props.time}</Text>
-
-      <Image
-        style={{
-          top: RFValue(75),
-          left: RFValue(25),
-          width: RFValue(50),
-          height: RFValue(50),
-        }}
-        source={require("./Rain.png")}
-      />
-
-      <Text style={styles.announcement}>{props.announcement}</Text>
-    </View>;
-  }
-}
+};
 
 const styles = StyleSheet.create({
-  header: {
-    display: "flex",
-    flexDirection: "row",
-    backgroundColor: "#EFC8C3",
-    alignItems: "center",
-    justifyContent: "center",
-    width: "100%",
-    height: RFValue(30),
-  },
-  headerSun: {
-    display: "flex",
-    flexDirection: "",
-    backgroundColor: "#E1AD01",
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "flex-start",
-    flexWrap: "wrap",
-    width: RFValue(250),
-    height: RFValue(250),
-    borderRadius: "50%",
-    bottom: RFValue(10),
-  },
-  textBubble: {
-    width: RFValue(150),
-    height: RFValue(100),
-    fontSize: RFValue(12),
-    flexWrap: "wrap",
-    paddingTop: RFValue(120),
-    paddingLeft: RFValue(10),
-  },
-  announcement: {
-    width: RFValue(200),
-    height: RFValue(100),
-    fontSize: RFValue(12),
-    paddingTop: RFValue(40),
-    paddingLeft: RFValue(30),
-  },
+    header: {
+        display: "flex",
+        flexDirection: "row",
+        backgroundColor: "#EFC8C3",
+        alignItems: "center",
+        justifyContent: "center",
+        width: "100%",
+        height: RFValue(30),
+    },
+    headerSun: {
+        display: "flex",
+        flexDirection: "",
+        backgroundColor: "#E1AD01",
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "flex-start",
+        flexWrap: "wrap",
+        width: RFValue(200),
+        height: RFValue(200),
+        borderRadius: "50%",
+        bottom: RFValue(10),
+    },
+    textBubble: {
+        width: RFValue(150),
+        height: RFValue(100),
+        fontSize: RFValue(10),
+        flexWrap: "wrap",
+        paddingTop: RFValue(100),
+        paddingLeft: RFValue(10),
+    },
+    announcement: {
+        width: RFValue(200),
+        height: RFValue(100),
+        fontSize: RFValue(10),
+        paddingTop: RFValue(40),
+        paddingLeft: RFValue(30),
+    },
+
+    placeholderWeather: {
+
+        backgroundColor: "#29E0CC",
+        width: RFValue(30),
+        height: RFValue(30),
+        top: 170,
+
+    },
+
+    placeholder: {
+        backgroundColor: "#29E0CC",
+        width: RFValue(30),
+        height: RFValue(30),
+    },
+
 });
+
+export default Header;

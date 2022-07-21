4587e515bc3227d3bd4f0d0ba3df3f60ebea02af
Lee Janzen-Morel <46356258+LeeJMorel@users.noreply.github.com>
2022-07-20 18:14:01 -0700

    Create Card.js
    Signed-off-by: Lee Janzen-Morel <46356258+LeeJMorel@users.noreply.github.com>

---
+++ solala-app/components/Card.js
@@ -0,0 +1,180 @@
+import React from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  Pressable,
+  FlatList,
+  SafeAreaView,
+  StatusBar,
+} from "react-native";
+import { RFValue } from "react-native-responsive-fontsize";
+
+import * as Favicon from "../../assets/favicons_js";
+
+import { theme } from "../constants";
+const { light, size, text, shadowProp } = theme;
+
+const DATA = [
+  {
+    id: "bd7acbea-c1b1-46c2-aed5-3ad53abb28ba",
+    title: "First Item",
+  },
+  {
+    id: "3ac68afc-c605-48d3-a4f8-fbd91aa97f63",
+    title: "Second Item",
+  },
+  {
+    id: "58694a0f-3da1-471f-bd96-145571e29d72",
+    title: "Third Item",
+  },
+];
+
+const Item = ({ title }) => (
+  <View style={cardStyles.cardItem}>
+    <View style={cardStyles.cardObjectLeft}>
+      <Text style={cardStyles.cardObjectText}>8 AM</Text>
+    </View>
+    <View style={cardStyles.cardObjectRight}>
+      <Text style={cardStyles.cardObjectText}>{title}</Text>
+    </View>
+  </View>
+);
+
+/* green bubble for menus */
+const Card = () => {
+  const renderItem = ({ item }) => <Item title={item.title} />;
+
+  const handleAddObject = () => {
+    console.log("Add todo or event");
+  };
+
+  return (
+    <SafeAreaView style={cardStyles.card}>
+      <View style={cardStyles.cardHeader}>
+        <View style={cardStyles.cardHeaderLeft} />
+        <View style={cardStyles.cardHeaderCenter}>
+          <Text style={cardStyles.cardHeaderText}>Hello</Text>
+        </View>
+        <View style={cardStyles.cardHeaderRight}>
+          <Pressable
+            onPress={() => {
+              handleAddObject;
+            }}
+          >
+            <Text>stuff</Text>
+          </Pressable>
+        </View>
+      </View>
+
+      <View style={{ alignSelf: "stretch" }}>
+        <FlatList
+          data={DATA}
+          renderItem={renderItem}
+          keyExtractor={(item) => item.id}
+          ListFooterComponent={
+            <View style={cardStyles.scrollDown}>
+              <Pressable
+                onPress={() => {
+                  handleAddObject;
+                }}
+              >
+                <Favicon.ScrollDown />
+              </Pressable>
+            </View>
+          }
+        />
+      </View>
+    </SafeAreaView>
+  );
+};
+//F
+
+const icon_size = 75;
+export const cardStyles = StyleSheet.create({
+  card: {
+    backgroundColor: light.secondary,
+    flexDirection: "column",
+    alignItems: "center",
+    borderRadius: size.borderRadius,
+    opacity: 0.7,
+    flex: 1,
+    paddingBottom: size.padding,
+    ...shadowProp,
+    marginTop: StatusBar.currentHeight || 0,
+  },
+
+  cardHeader: {
+    alignSelf: "stretch",
+    borderRadius: size.borderRadius,
+    backgroundColor: light.accent,
+    opacity: 0.7,
+    justifyContent: "space-between",
+    flexDirection: "row",
+    textAlign: "center",
+  },
+
+  cardHeaderLeft: {
+    flex: 1,
+  },
+
+  cardHeaderCenter: {
+    flex: 10,
+  },
+
+  cardHeaderRight: {
+    flex: 1,
+  },
+
+  cardHeaderText: {
+    ...text.title,
+    color: light.textPrimary,
+  },
+
+  cardItem: {
+    paddingVertical: size.innerPadding,
+    paddingRight: size.innerPadding,
+    marginHorizontal: size.margin,
+    marginTop: size.margin,
+    backgroundColor: light.primary,
+    borderRadius: size.borderRadius,
+    padding: size.padding,
+    justifyContent: "space-between",
+    flexDirection: "row",
+    textAlign: "center",
+    alignSelf: "stretch",
+    flex: 1,
+  },
+
+  cardObjectLeft: {
+    /*checkbox bubble, time, reset, ect */
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "flex-start",
+  },
+
+  cardObjectRight: {
+    flex: 8,
+    alignItems: "flex-start",
+    justifyContent: "center",
+  },
+
+  cardObjectText: {
+    ...text.body,
+    fontColor: light.textSecondary,
+  },
+
+  scrollDown: {
+    alignItems: "center",
+    justifyContent: "center",
+    paddingTop: size.padding,
+  },
+
+  button: {
+    justifyContent: "space-around",
+    flexDirection: "column",
+    alignItems: "center",
+  },
+});
+
+export default Card;

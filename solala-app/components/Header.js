103f8859a36e36961ea0512ca515278b223b615e
Lee Janzen-Morel <leejanzen91@gmail.com>
2022-07-21 14:23:22 -0700

    resolved whitespace

--- solala-app/components/Header.js
+++ solala-app/components/Header.js
@@ -1,5 +1,11 @@
 import React from "react";
-import { Text, View, StyleSheet, TouchableOpacity, Platform } from "react-native";
+import {
+  Text,
+  View,
+  StyleSheet,
+  TouchableOpacity,
+  Platform,
+} from "react-native";
 
 import GreenHouse from "../../assets/favicons_light/greenhouse.svg";
 import Calendar from "../../assets/favicons_light/calendar.svg";
@@ -12,120 +18,105 @@ import * as Weather from "../../assets/favicons_weather";
 const { colorPalette } = theme;
 
 const Header = (props) => {
+  const CalendarPressEvent = () => {
+    console.log("Temp calendar navigation");
+  };
+  const ToDoPressEvent = () => {
+    console.log("Temp TODO navigation");
+  };
 
+  if (Platform.OS === "android" || Platform.OS === "IOS") {
+    return (
+      <View style={styles.headerBox}>
+        <View style={styles.header}>
+          <View style={styles.headerSun}>
+            <Text style={styles.textBubble}>{props.greeting}</Text>
+            <View style={styles.placeholderWeather}></View>
+            <Text style={styles.announcement}>{props.announcement}</Text>
+          </View>
+        </View>
+      </View>
+    );
+  } else {
+    return (
+      <View style={styles.headerBox}>
+        <View style={styles.header}>
+          <TouchableOpacity onPress={CalendarPressEvent}>
+            <View style={styles.placeholder}></View>
+          </TouchableOpacity>
 
-    
-    const CalendarPressEvent = () => {
-        console.log("Temp calendar navigation");
-    };
-    const ToDoPressEvent = () => {
-        console.log("Temp TODO navigation");
-    };
+          <View style={styles.headerSun}>
+            <Text style={styles.textBubble}>{props.greeting}</Text>
+            <View style={styles.placeholderWeather}></View>
+            <Text style={styles.announcement}>{props.announcement}</Text>
+          </View>
 
-
-
-    if (Platform.OS === "android" || Platform.OS === "IOS") {
-
-        return (
-           
-            <View style={styles.headerBox}>
-                <View style={styles.header}>
-
-                    <View style={styles.headerSun}>
-                        <Text style={styles.textBubble}>{props.greeting}</Text>
-                        <View style={styles.placeholderWeather}></View>
-                        <Text style={styles.announcement}>{props.announcement}</Text>
-                    </View>
-
-                </View>
-            </View>
-                 );
-       
-    } else {
-        return (
-
-            <View style={styles.headerBox}>
-                
-                <View style={styles.header}>
-
-                    <TouchableOpacity onPress={CalendarPressEvent}>
-                        <View style={styles.placeholder}></View>
-                    </TouchableOpacity>
-
-                    <View style={styles.headerSun}>
-                        <Text style={styles.textBubble}>{props.greeting}</Text>
-                        <View style={styles.placeholderWeather}></View>
-                        <Text style={styles.announcement}>{props.announcement}</Text>
-                    </View>
-
-                    <TouchableOpacity onPress={ToDoPressEvent}>
-                        <View style={styles.placeholder}></View>
-                    </TouchableOpacity>
-
-                </View>
-            </View>
-         );
-    }
+          <TouchableOpacity onPress={ToDoPressEvent}>
+            <View style={styles.placeholder}></View>
+          </TouchableOpacity>
+        </View>
+      </View>
+    );
+  }
 };
 
 const styles = StyleSheet.create({
-    header: {
-        display: "flex",
-        flexDirection: "row",
-        backgroundColor: theme.colorPalette.pink,
-        alignItems: "center",
-        justifyContent: "center",
-        width: "100%",
-        height: RFValue(30),
-    },
-    headerSun: {
-        display: "flex",
-        flexDirection: "",
-        backgroundColor: theme.colorPalette.goldenrod,
-        flexDirection: "row",
-        alignItems: "center",
-        justifyContent: "flex-start",
-        flexWrap: "wrap",
-        width: RFValue(200),
-        height: RFValue(200),
-        borderRadius: "50%",
-        bottom: RFValue(10),
-    },
-    headerBox: {
-        height: RFValue(120),
-        width: "100%",
-    },
-    textBubble: {
-        width: RFValue(150),
-        height: RFValue(100),
-        fontSize: RFValue(10),
-        flexWrap: "wrap",
-        paddingTop: RFValue(100),
-        paddingLeft: RFValue(10),
-        fontFamily: theme.fonts.title,
-    },
-    announcement: {
-        width: RFValue(200),
-        height: RFValue(100),
-        fontSize: RFValue(10),
-        paddingTop: RFValue(40),
-        paddingLeft: RFValue(15),
-        fontFamily: theme.fonts.body,
-    },
+  header: {
+    display: "flex",
+    flexDirection: "row",
+    backgroundColor: theme.colorPalette.pink,
+    alignItems: "center",
+    justifyContent: "center",
+    width: "100%",
+    height: RFValue(30),
+  },
+  headerSun: {
+    display: "flex",
+    flexDirection: "",
+    backgroundColor: theme.colorPalette.goldenrod,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "flex-start",
+    flexWrap: "wrap",
+    width: RFValue(200),
+    height: RFValue(200),
+    borderRadius: "50%",
+    bottom: RFValue(10),
+  },
+  headerBox: {
+    height: RFValue(120),
+    width: "100%",
+  },
+  textBubble: {
+    width: RFValue(150),
+    height: RFValue(100),
+    fontSize: RFValue(10),
+    flexWrap: "wrap",
+    paddingTop: RFValue(100),
+    paddingLeft: RFValue(10),
+    fontFamily: theme.fonts.title,
+  },
+  announcement: {
+    width: RFValue(200),
+    height: RFValue(100),
+    fontSize: RFValue(10),
+    paddingTop: RFValue(40),
+    paddingLeft: RFValue(15),
+    fontFamily: theme.fonts.body,
+  },
 
-    placeholderWeather: {
-        backgroundColor: theme.colorPalette.teal,
-        width: RFValue(30),
-        height: RFValue(30),
-        top: 170,
-    },
+  placeholderWeather: {
+    backgroundColor: theme.colorPalette.teal,
+    width: RFValue(30),
+    height: RFValue(30),
+    top: 170,
+  },
 
-    placeholder: {
-        backgroundColor: theme.colorPalette.teal,
-        width: RFValue(30),
-        height: RFValue(30),
-    },
-
+  placeholder: {
+    backgroundColor: theme.colorPalette.teal,
+    width: RFValue(30),
+    height: RFValue(30),
+  },
 });
 
 export default Header;

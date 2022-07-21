377f2080528dad640fabb14b8763fc00ab1ebbe8
Epicplant <108161060+Epicplant@users.noreply.github.com>
2022-07-20 18:19:41 -0700

    Update header.js
    Signed-off-by: Epicplant <108161060+Epicplant@users.noreply.github.com>

--- solala-app/components/header.js
+++ solala-app/components/header.js
@@ -1,71 +1,112 @@
-import React, { useState } from "react";
-import { Text, View, TextInput, Button, Platform } from "react-native";
-import { Ionicons } from "@expo/vector-icons";
+
+
+import React from "react";
+import { StyleSheet, Text, View, Image, TouchableOpacity, Platform } from "react-native";
 import { RFValue } from "react-native-responsive-fontsize";
-import { theme } from "../constants";
 
 
-const { colors, size, text, shadowProp } = theme;
 
-export default function header() {
+export default function Header(props) {
+    let staticImage = require('./Rain.png');
 
-    if (Platform.OS === 'android') {
-        //phone screen
-        return (<View>
-            <StatusBar
-                animated={true}
-                backgroundColor="#61dafb"
-                barStyle={statusBarStyle}
-                showHideTransition={statusBarTransition} />
-            <View style={styles.headerSun}>
-                <Text>{this.props.timeOfDay} {this.props.message}</Text>
-                <Text>{this.props.notification}</Text>
+    if (Platform.OS === 'web') {
+        return (
+
+            <View style={styles.header}>
+
+                <TouchableOpacity>
+                    <Image
+                        style={{
+                            width: RFValue(25),
+                            height: RFValue(25),
+                        }} source={require('./calendar.png')} />
+                </TouchableOpacity>
+
+                <View style={styles.headerSun}>
+                    <Text style={styles.textBubble}>{props.time}</Text>
+
+                    <Image
+                        style={{
+                            top: RFValue(75),
+                            left: RFValue(25),
+                            width: RFValue(50),
+                            height: RFValue(50),
+                        }} source={require('./Rain.png')} />
+
+                    <Text style={styles.announcement}>{props.announcement}</Text>
+
+                </View>
+
+                <TouchableOpacity>
+                    <Image
+                        style={{
+                            width: RFValue(25),
+                            height: RFValue(25),
+                        }} source={require('./TODO.png')} />
+                </TouchableOpacity>
+
             </View>
-        </View>);
-    } else {
-        //desktop screen
 
-        return (<View style={styles.desktopHeader}>
-            <View style={styles.headerSun}>
-                <Text>{this.props.timeOfDay} {this.props.message}</Text>
-                <Text>{this.props.notification}</Text>
-            </View>
-        </View>);
+
+        );
+    } else if (Platform.OS === 'android') {
+        <View style={styles.headerSun}>
+            <Text style={styles.textBubble}>{props.time}</Text>
+
+            <Image
+                style={{
+                    top: RFValue(75),
+                    left: RFValue(25),
+                    width: RFValue(50),
+                    height: RFValue(50),
+                }} source={require('./Rain.png')} />
+
+            <Text style={styles.announcement}>{props.announcement}</Text>
+
+        </View>
     }
 }
 
 
 const styles = StyleSheet.create({
-    desktopHeader: {
-        backgroundColor: colors.header.primary,
+    header: {
+        display: "flex",
+        flexDirection: "row",
+        backgroundColor: "#EFC8C3",
+        alignItems: "center",
+        justifyContent: "center",
         width: "100%",
-        display: flex,
-        alignItems: center,
-        justifyContent: center,
-        height: 75,
-
-
+        height: RFValue(30),
     },
-
-    phoneHeader: {
-        backgroundColor: colors.header.primary,
-        width: "100%",
-        display: flex,
-        alignItems: center,
-        justifyContent: center,
-        height: 75,
-    },
-
     headerSun: {
-
-        width: 100,
-        height: 100,
+        display: "flex",
+        flexDirection: "",
+        backgroundColor: "#E1AD01",
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "flex-start",
+        flexWrap: "wrap",
+        width: RFValue(250),
+        height: RFValue(250),
         borderRadius: "50%",
-        backgroundColor: colors.header.sun,
-        fontSize: RFValue(18),
+        bottom: RFValue(10),
 
-        //To display image as a circle using CSS, Set width and height properties for image
-        //with same CSS length value.Refer CSS Length Units.Set border-radius property with a value of 50%.
-    }
+    },
+    textBubble: {
+        width: RFValue(150),
+        height: RFValue(100),
+        fontSize: RFValue(12),
+        flexWrap: "wrap",
+        paddingTop: RFValue(120),
+        paddingLeft: RFValue(10),
 
+    },
+    announcement: {
+        width: RFValue(200),
+        height: RFValue(100),
+        fontSize: RFValue(12),
+        paddingTop: RFValue(40),
+        paddingLeft: RFValue(30),
+
+    },
 });

57bf7ae7cbfe68b3000dbe5cde8fb249d3663c50
Lee Janzen-Morel <leejanzen91@gmail.com>
2022-07-22 18:09:03 -0700

    added login

--- solala-app/screens/app/index.js
+++ solala-app/screens/app/index.js
@@ -1,3 +1,4 @@
 export { default as Homepage } from "./homepage";
 export { default as Calendar } from "./calendar";
 export { default as Todo } from "./todo";
+export { default as Login } from "./login";

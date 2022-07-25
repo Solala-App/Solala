e967873d0a7d407a867ceec9b4ead3fcf0315d9d
Lee Janzen-Morel <leejanzen91@gmail.com>
2022-07-25 11:32:15 -0700

    change to splashscreen

--- App.tsx
+++ App.tsx
@@ -1,25 +1,43 @@
 import { StatusBar } from "expo-status-bar";
-import React, { useState } from "react";
+import React, { useState, useCallback, useEffect } from "react";
 import * as Screens from "./solala-app/screens";
 import { View, Text } from "react-native";
 import fonts from "./solala-app/constants";
 import AppLoading from "expo-app-loading";
+import * as SplashScreen from "expo-splash-screen";
 
 export default function App() {
-  const [IsReady, SetIsReady] = useState(false);
+  const [appIsReady, setAppIsReady] = useState(false);
 
-  const LoadFontsAndRestoreToken = async () => {
-    await fonts();
-  };
+  useEffect(() => {
+    async function prepare() {
+      try {
+        // Pre-load fonts, make any API calls you need to do here
+        await fonts;
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        // Tell the application to render
+        setAppIsReady(true);
+      }
+    }
 
-  if (!IsReady) {
-    return (
-      <AppLoading
-        startAsync={LoadFontsAndRestoreToken}
-        onFinish={() => SetIsReady(true)}
-        onError={() => {}}
-      />
-    );
+    prepare();
+  }, []);
+
+  const onLayoutRootView = useCallback(async () => {
+    if (appIsReady) {
+      // This tells the splash screen to hide immediately! If we call this after
+      // `setAppIsReady`, then we may see a blank screen while the app is
+      // loading its initial state and rendering its first pixels. So instead,
+      // we hide the splash screen once we know the root view has already
+      // performed layout.
+      await SplashScreen.hideAsync();
+    }
+  }, [appIsReady]);
+
+  if (!appIsReady) {
+    return null;
   }
   //add a splashscreen
   return <Screens.App.Homepage />;
--- solala-app/components/Card.js
+++ solala-app/components/Card.js
@@ -9,16 +9,15 @@ import {
   StatusBar,
   TouchableOpacity,
   Modal,
-  ScrollView
+  ScrollView,
 } from "react-native";
 import { RFValue } from "react-native-responsive-fontsize";
-import Slider from '@react-native-community/slider';
-import { Dropdown } from 'react-native-element-dropdown';
+import Slider from "@react-native-community/slider";
+import { Dropdown } from "react-native-element-dropdown";
 import * as Favicon from "../../assets/favicons_js";
-import AddTaskPopup from './AddTaskPopup.js'
+import AddTaskPopup from "./AddTaskPopup.js";
 import { theme } from "../constants";
 const { light, size, text, shadowProp } = theme;
-
 
 // npm install @react-native-community/slider --save
 // yarn add react-native-select-dropdown
@@ -51,12 +50,11 @@ const Item = ({ title }) => (
 
 /* green bubble for menus */
 const Card = () => {
-    const renderItem = ({ item }) => <Item title={item.title} />;
-    const [isModalVisible, setIsModalVisible] = React.useState(false);
+  const renderItem = ({ item }) => <Item title={item.title} />;
+  const [isModalVisible, setIsModalVisible] = React.useState(false);
 
-
-    const handleAddObject = () => {
-        setIsModalVisible(() => !isModalVisible);      
+  const handleAddObject = () => {
+    setIsModalVisible(() => !isModalVisible);
   };
 
   //reaserch needed...we also need a scroll up...how do these arrows work with flatbox?
@@ -71,15 +69,15 @@ const Card = () => {
         <View style={cardStyles.cardHeaderCenter}>
           <Text style={cardStyles.cardHeaderText}>Hello</Text>
         </View>
-          <View style={cardStyles.cardHeaderRight}>
-                  <TouchableOpacity onPress={handleAddObject}>
-                      <Favicon.Plus style={{ width: 44 }} />
-              </TouchableOpacity>
+        <View style={cardStyles.cardHeaderRight}>
+          <TouchableOpacity onPress={handleAddObject}>
+            <Favicon.Plus style={{ width: 44 }} />
+          </TouchableOpacity>
 
-                  <Modal visible={isModalVisible} transparent={true}>
-                      <AddTaskPopup isModalVisible={handleAddObject} />
-                </Modal>
-          </View>
+          <Modal visible={isModalVisible} transparent={true}>
+            <AddTaskPopup isModalVisible={handleAddObject} />
+          </Modal>
+        </View>
       </View>
 
       <View style={{ alignSelf: "stretch" }}>
@@ -177,7 +175,7 @@ export const cardStyles = StyleSheet.create({
 
   cardObjectText: {
     ...text.body,
-    fontColor: light.textSecondary,
+    color: light.textSecondary,
   },
 
   scrollDown: {
@@ -189,13 +187,13 @@ export const cardStyles = StyleSheet.create({
     justifyContent: "space-around",
     flexDirection: "column",
     alignItems: "center",
-    },
+  },
 
-    centeredView: {
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-    }
+  centeredView: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
 });
 
 export default Card;

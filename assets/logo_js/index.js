5886f5f2e764748c2099f93d9953fec0c8075fc4
Lee Janzen-Morel <46356258+LeeJMorel@users.noreply.github.com>
2022-07-30 16:52:37 -0700

    Delete assets/logo_js directory
    Signed-off-by: Lee Janzen-Morel <46356258+LeeJMorel@users.noreply.github.com>

--- assets/logo_js/index.js
+++
@@ -1,5 +0,0 @@
-export { default as SolalaLogoLg } from "./SolalaLogoLg.js";
-export { default as SolalaLogo } from "./SolalaLogo.js";
-export { default as SolalaLogoText } from "./SolalaLogoText.js";
-export { default as SolalaFlower } from "./SolalaFlower.js";
-export { default as SolalaIcon } from "./SolalaIcon.js";

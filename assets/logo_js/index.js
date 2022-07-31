a34c679eb02389248b6fbc1e06e88fc7ae52d64e
Lee Janzen-Morel <leejanzen91@gmail.com>
2022-07-30 17:16:15 -0700

    more name fixes

---
+++ assets/logo_js/index.js
@@ -0,0 +1,5 @@
+export { default as SolalaLogoLg } from "./SolalaLogoLg.js";
+export { default as SolalaLogo } from "./SolalaLogo.js";
+export { default as SolalaLogoText } from "./SolalaLogoText.js";
+export { default as SolalaFlower } from "./SolalaFlower.js";
+export { default as SolalaIcon } from "./SolalaIcon.js";
---
+++ assets/solala_js/index.js
@@ -0,0 +1,1 @@
+export { default as Happy } from "./happy.js";

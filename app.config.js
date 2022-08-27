7ac52c89f82e384a02e1f3a6f0133a7d0d41b3b2
Lee Janzen-Morel <leejanzen91@gmail.com>
2022-08-27 11:05:30 -0700

    replace date time picker for easier bug resolve

--- app.config.js
+++ app.config.js
@@ -30,15 +30,13 @@ export default {
       backgroundColor: "#E3FFEB",
       favicon: "./assets/logo/SolalaIcon.png",
     },
-    firebase: {
+    extra: {
       firebaseApiKey: process.env.FIREBASE_API_KEY,
       firebaseAuthDomain: process.env.FIREBASE_AUTH_DOMAIN,
       firebaseProjectId: process.env.FIREBASE_PROJECT_ID,
       firebaseStorageBucket: process.env.FIREBASE_STORAGE_BUCKET,
       firebaseMessagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
       firebaseAppId: process.env.FIREBASE_APP_ID,
-    },
-    extra: {
       openWeatherKey: process.env.OPEN_WEATHER_KEY,
     },
   },
